a61c276940cad07ac7b4c32f6f02e488cb833d39
NaveenBandarage <thebanterage@gmail.com>
2020-02-04 22:59:02 +1300

    added loop.

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ import (
 type StockInfo struct {
 	Symbol        string  `json:"symbol"`
 	LastSalePrice float32 `json:"lastSalePrice"`
+	LastUpdated   int64   `json:"lastUpdated"`
 }
 
 //Structure fot the stock info.
 type Stocks []StockInfo
 
 func main() {
+
 	stockName := "&symbols=aapl"
 
 	apiKey, err := ioutil.ReadFile("api.txt")
@@ -28,25 +30,27 @@ func main() {
 	}
 
 	base := "https://sandbox.iexapis.com/stable/tops?token="
-	response, err := http.Get(base + string(apiKey) + stockName)
+	for i := 1; i <= 10; i++ {
+		response, err := http.Get(base + string(apiKey) + stockName)
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(string(responseData))
+		var stockVar Stocks
+
+		jsonFile := json.Unmarshal([]byte(responseData), &stockVar)
+
+		if jsonFile != nil {
+			fmt.Println("error:", jsonFile)
+		}
+
+		fmt.Println(stockVar[0].Symbol, stockVar[0].LastSalePrice, stockVar[0].LastUpdated)
 	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
-	var stockVar Stocks
-
-	jsonFile := json.Unmarshal([]byte(responseData), &stockVar)
-
-	if jsonFile != nil {
-		fmt.Println("error:", jsonFile)
-	}
-	fmt.Println(stockVar[0].Symbol, stockVar[0].LastSalePrice)
-
 }
